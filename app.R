3ec0735aef002c90b87507ca7d390020f1054f89
lnicholas-c <lnicholas-c@shire.com>
2017-07-05 17:55:19 +0000

    lnicholas-c changes

--- app.R
+++ app.R
@@ -6,14 +6,14 @@ motorVehicleDeaths <- motorVehicleDeaths[2:53, ]
 ui <- dashboardPage(
   
   skin = "black",
-    dashboardHeader(title = "Motor Vehicle Occupant Death Rate By Age: 2012 and 2014", titleWidth = 250),
-      dashboardSidebar( width = 350,
+    dashboardHeader(title = "Motor Vehicle Occupant Death Rate By Age", titleWidth = 450),
+      dashboardSidebar( width = 250,
         sidebarMenu(
-          menuItem("All Ages Plot", tabName = "allAges", icon = icon("allAges")),
-          menuItem("Ages 0 - 20", tabName = "ages0-20", icon = icon("ages0-20")),
-          menuItem("Ages 21 - 34", tabName = "ages21-34", icon = icon("ages21-34")),
-          menuItem("Ages 35 - 54", tabName = "ages35-54", icon = icon("ages35-54")),
-          menuItem("Ages 55 and Older", tabName = "ages55+", icon = icon("ages55+")),
+          menuItem("All Ages", tabName = "allAges", icon = icon("allAges")),
+          menuItem("Ages 0 - 20", tabName = "ages0", icon = icon("ages0")),
+          menuItem("Ages 21 - 34", tabName = "ages21", icon = icon("ages21")),
+          menuItem("Ages 35 - 54", tabName = "ages35", icon = icon("ages35")),
+          menuItem("Ages 55 and Older", tabName = "ages55", icon = icon("ages55")),
           menuItem("Data Table", tabName = "dataTable", icon = icon("DataTable"))
         
         )
@@ -23,52 +23,91 @@ ui <- dashboardPage(
      tabItems(
        tabItem( tabName = "allAges",
                 fluidRow(
-                  box(plotOutput("plotAllAges"))
-                ) 
-         
+                  box(plotOutput("plotAllAges")
+                  )
+                )
               ),
        
-       tabItem( tabName = "ages0-20", 
+       tabItem( tabName = "ages0", 
                 fluidRow(
-                  box(plotOutput(("plotAges0-24"))
+                  box(plotOutput("plotAges0")
+                  )
                 )
-                
               ),
               
-       tabItem( tabName = "ages21-34", 
+       tabItem( tabName = "ages21", 
                 fluidRow(
-                  box(plotOutput("plotAges21-34"))
+                  box(plotOutput("plotAges21")
+                  )
                 )
-                
               ),
        
-       tabItem( tabName = "ages35-54", 
+       tabItem( tabName = "ages35", 
                 fluidRow(
-                  box(plotOutput("plotAges35-54"))
+                  box(plotOutput("plotAges35")
+                  )
                 )
-                
               ),
        
-       tabItem( tabName = "ages55+",
+       tabItem( tabName = "ages55",
                 fluidRow(
-                  box(plotOutput("plotAges55+"))
+                  box(plotOutput("plotAges55")
+                  )
                 )
-              
               ),
        
        tabItem( tabName = "dataTable",
                 fluidRow(
-                  box(tableOutput("table"))
-          )
-        )
-      )
+                  box(dataTableOutput("table")
+                  )
+                )        
+              )
     )
   )
 )
 
-  
+
 server <- function( input, output ){
   
+  output$plotAllAges <- renderPlot({
+    plot( motorVehicleDeaths$All.Ages..2012, motorVehicleDeaths$All.Ages..2014,
+          xlab = "Deaths Among All Ages: 2012", ylab = "Deaths Among All Ages: 2014", 
+          main = "All Ages: 2012 vs. 2014")
+    
+  })
+  
+  output$plotAges0 <- renderPlot({
+    plot( motorVehicleDeaths$Age.0.20..2012, motorVehicleDeaths$Age.0.20..2014,
+          xlab = "Deaths Among People Ages 0-20: 2012", ylab = "Deaths Among People Ages 0-20: 2014", 
+          main = "Ages 0 - 20: 2012 vs. 2014")
+    
+  })
+  
+  output$plotAges21 <- renderPlot({
+    plot( motorVehicleDeaths$Age.21.34..2012, motorVehicleDeaths$Age.21.34..2014,
+          xlab = "Deaths Among People Ages 21-34: 2012", ylab = "Deaths Among People Ages 21-34: 2014", 
+          main = "Ages 21 - 34: 2012 vs. 2014")
+    
+    
+  })
+  
+  output$plotAges35 <- renderPlot({
+    plot( motorVehicleDeaths$Age.35.54..2012, motorVehicleDeaths$Age.35.54..2014,
+          xlab = "Deaths Among People Ages 35-54: 2012", ylab = "Deaths Among People Ages 35-54: 2014", 
+          main = "Ages 35 - 54: 2012 vs. 2014")
+    
+  })
+  
+  output$plotAges55 <- renderPlot({
+    plot( motorVehicleDeaths$Age.55...2012, motorVehicleDeaths$Age.55...2014,
+          xlab = "Deaths Among People Ages 55 And Older: 2012", ylab = "Deaths Among People Ages 55 And Older: 2014", 
+          main = "Ages 55 And Odler: 2012 vs. 2014")
+    
+  })
+  
+  output$table <- renderDataTable({
+    motorVehicleDeaths
+  })
   
 }
 
